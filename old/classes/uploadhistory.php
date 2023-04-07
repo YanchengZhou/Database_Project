49a6bb02b2169d01440b57b07540fae0e095a21f
Hengzhou Li <91091681+LKT123@users.noreply.github.com>
2023-04-07 19:59:00 -0400

    Delete old directory

--- old/classes/uploadhistory.php
+++
@@ -1,9 +0,0 @@
-<?php
-    spl_autoload_register(function($classname) {
-        include "$classname.php";
-    });
-
-    $db = new Database();
-    $allsubmissions = $db->query("select * from uploadhistory");
-    $jsonObj = json_encode($allsubmissions);
-    echo $jsonObj;
