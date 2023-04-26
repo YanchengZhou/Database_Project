603df0a23bcfd91ae53368dcf7f18fb1ff5bfa6e
hw5ujv <89873159+hw5ujv@users.noreply.github.com>
2023-04-25 20:29:58 -0400

    分离navbar

--- index.php
+++ index.php
@@ -34,43 +34,7 @@
 
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="index.php">Home <span class="sr-only"></span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Used Item</a>
-            </li>
-
-            <li class="nav-item">
-                <a class="nav-link" href="#">House Rental</a>
-            </li>
-
-            <li class="nav-item">
-                <a class="nav-link" href="#">Carpooling</a>
-            </li>
-
-            <li class="nav-item">
-                <a class="nav-link" href="#">Account</a>
-            </li>
-
-        </ul>
-
-    </div>
-
-    <li class="nav-item">
-        <a href="upload.php"><button class="btn btn-primary ml-auto mr-2" type="button">Upload</button></a>
-        <a href="login.php"><button class="btn btn-primary ml-auto mr-2" type="button">Log In</button></a>
-        <a href="signup.php"><button class="btn btn-primary ml-auto mr-2" type="button">Sign Up</button></a>
-    </li>
-</nav>
+<?php include 'navbar.php'; ?>
 
 <form action="index.php" method="get">
     <input type="text" name="query" placeholder="Search...">
--- upload.php
+++ upload.php
@@ -113,42 +113,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="index.php">Home <span class="sr-only"></span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Used Item</a>
-            </li>
-
-            <li class="nav-item">
-                <a class="nav-link" href="#">House Rental</a>
-            </li>
-
-            <li class="nav-item">
-                <a class="nav-link" href="#">Carpooling</a>
-            </li>
-
-            <li class="nav-item">
-                <a class="nav-link" href="#">Account</a>
-            </li>
-
-        </ul>
-
-    </div>
-
-    <li class="nav-item">
-        <button class="btn btn-primary ml-auto mr-2" type="button">Upload</button>
-        <button class="btn btn-primary ml-auto mr-2" type="button">Log In</button>
-    </li>
-</nav>
+<?php include 'navbar.php'; ?>
 
 <div class="container">
     <div class="row">
